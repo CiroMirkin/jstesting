b5edf7cb297808594c0247e7ebb47b53c7809150
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2022-12-03 17:00:05 +0000

    changes in print function

--- jstesting.js
+++ jstesting.js
@@ -1,13 +1,13 @@
 "use strict"
 
 const printTestResult = ({ testDescription, functionOutput, testPassed }) => {
-  console.log(`-------------------------\n 
-  ${
-    !testDescription ? (testPassed ? 'n o n e' : "none, you're a *****") : testDescription
-  }
-  \n-------------------------\n${
-  testPassed ? 'Your function did a good work :)\n> The funtion returned' : "Your function is not working :)\n> The function does not return"
-  } ${functionOutput}
+  console.log(`${
+    testPassed ? 'Your function did a GOOD WORK :)' : "Your function is NOT WORKING :|"
+  }\n> ${
+    !testDescription ? (testPassed ? 'no description' : "none, you're a *****") : testDescription
+  }\n> ${
+    testPassed ? 'The funtion returned' : 'The function does not return'
+  }: ${functionOutput}
   `)
 }
 
@@ -24,4 +24,11 @@ const test = (testDescription, { functionToTesting, functionOutput }) => {
   return false
 }
 
-export default test
+const sum = (a, b) => a+b
+
+test('', {
+  functionToTesting: sum(2, 2), functionOutput: 4
+})
+test('Al sumar 10 mas quice el resultado es 15.', {
+  functionToTesting: sum(10, 5), functionOutput: 15
+})
