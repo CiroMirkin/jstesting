289f2b04e808b902ee8acadd4db2488640fa5a39
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2023-02-25 10:18:00 -0300

    cambio: la funcion que muestra los resultados de un test se partio en dos

--- jstesting.js
+++ jstesting.js
@@ -2,32 +2,36 @@
 
 let devMode = true;
 
-const printTestResult = ({ testDescription, functionOutput, testPassed }) => {
-	console.log(`${testPassed ? 'Your function did a GOOD WORK :)' : "Your function is NOT WORKING :|"}\n`)
-  console.log(`> ${!testDescription ? (testPassed ? 'no description' : "none, you're a *****") : testDescription}\n`)
-  console.log(`> ${testPassed ? 'The funtion returned' : 'The function does not return'}: ${functionOutput}`)
+const printGreenTestResult = ({ testDescription, functionOutput }) => {
+	console.log(`\nGOOD work\n`)
+  console.log(`> ${!testDescription ? 'no description': testDescription}\n`)
+  console.log(`> 'The funtion returned:' ${functionOutput}`)
 }
-
+const printRedTestResult = ({ testDescription, functionOutput }) => {
+	console.log(`\nBAD work\n`)
+  console.log(`> ${!testDescription ? "none, you're a *****": testDescription}\n`)
+  console.log(`> 'The function doesnt return' ${functionOutput}`)
+}
 
 const test = (testDescription, { functionOutput, expectFunctionOutput }) => {
   if(!devMode) return true
 
   if(typeof functionOutput == 'object' || typeof expectFunctionOutput == 'object') {
     if(`${functionOutput}` == `${expectFunctionOutput}`) {
-      printTestResult({
-        testDescription, expectFunctionOutput, testPassed: true
+      printGreenTestResult({
+        testDescription, expectFunctionOutput
       })
       return true
     }
   }
   if(functionOutput === expectFunctionOutput) {
-    printTestResult({
-      testDescription, expectFunctionOutput, testPassed: true
+    printGreenTestResult({
+      testDescription, expectFunctionOutput
     })
     return true
   }
-  printTestResult({
-      testDescription, expectFunctionOutput, testPassed: false
+  printRedTestResult({
+      testDescription, expectFunctionOutput
     })
   return false
 }
